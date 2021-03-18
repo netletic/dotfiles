51eb716c5ec9ad0e1b0f765bc465e411d9d674ee
netletic <netletic@gmail.com>
2021-03-18 01:51:55 +0100

    fonts, zsh and nvim update

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+########
+# nvim #
+########
+
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim/undo"
+
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+
+#########
+# Fonts #
+#########
+
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#######
+# Zsh #
+#######
+
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
