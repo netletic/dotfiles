d2d5437522ca3747ff49aa6014b595b007a02e44
netletic <netletic@gmail.com>
2021-04-06 20:28:03 +0200

    added .bashrc, .bash_profile and .bash_aliases

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 # Fonts #
 #########
 
-mkdir -p "$XDG_DATA_HOME"
-cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+#mkdir -p "$XDG_DATA_HOME"
+#cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 
 #######
 # Zsh #
@@ -26,3 +26,10 @@ ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
 rm -rf "$HOME/.config/zsh/external"
 ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+
+##########
+#  Bash  #
+##########
+ln -sf "$HOME/dotfiles/bash/.bashrc" "$HOME"
+ln -sf "$HOME/dotfiles/bash/.bash_aliases" "$HOME"
+ln -sf "$HOME/dotfiles/bash/.bash_profile" "$HOME"
