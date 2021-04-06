7b46426041d04e4e880e7b27f5f51171bcc3be74
netletic <netletic@gmail.com>
2021-04-06 20:33:03 +0200

    added mirror for bash_aliases to zsh_aliases

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+ln -sf "$HOME/dotfiles/bash/.bash_aliases" "$HOME/.config/zsh/aliases"
 rm -rf "$HOME/.config/zsh/external"
 ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
 
